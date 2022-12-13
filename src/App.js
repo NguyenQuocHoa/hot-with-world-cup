f026085de5bef8e295f54ef37bfaf2e5c5a7f6a7
NguyenQuocHoa <nguyenquochoa2000tb2@gmail.com>
2022-12-13 16:03:53 +0700

    fix replay

--- src/App.js
+++ src/App.js
@@ -3,23 +3,8 @@ import React, { useState, useEffect } from "react";
 import { Button, Row, Col } from "antd";
 import { PlayCircleFilled } from "@ant-design/icons";
 import FlagCard from "./components/flagCard/FlagCard";
-import argentinaSrc from "./flag/argentina-flag-8x5.gif";
-import australiaSrc from "./flag/australia-flag-8x5-std.jpg";
-import belgiumSrc from "./flag/belgium--flag-8x5.gif";
-import franceSrc from "./flag/france-flag-8x5.gif";
-import germanySrc from "./flag/germany-flag-8x5.gif";
-import hollandSrc from "./flag/holland-flag-8x5.gif";
-import japanSrc from "./flag/japan-flag-8x5.gif";
-import polandSrc from "./flag/poland-flag-8x5.jpg";
-import portugalSrc from "./flag/portugak-flag-8x5.jpg";
-import koreaSrc from "./flag/s-korea-8x5-std-1_1.jpg";
-import spainSrc from "./flag/spain-flag-8x5.jpg";
-import englandSrc from "./flag/st-george-flag-8x5.gif";
-import switzerlandSrc from "./flag/switzerland-flag-8x5.jpg";
-import mexicoSrc from "./flag/mexico-flag-8x5.gif";
-import usaSrc from "./flag/usa-flag-8x5.gif";
-import walesSrc from "./flag/wales-flag-8x5.jpg";
 
+import { data as DATA } from "./data";
 const NUMBER_NATION_SUCCESS = 6;
 
 function App() {
@@ -27,110 +12,24 @@ function App() {
   const [span, setSpan] = useState(4);
   const [styles, setStyles] = useState({ fontSize: "0.9em", marginBottom: -26 });
   const [selectedId, setSelectedId] = useState(0);
-  const [nations, setNations] = useState([
-    {
-      id: 1,
-      name: "argentina",
-      isSuccess: false,
-      srcImg: argentinaSrc,
-    },
-    {
-      id: 2,
-      name: "australia",
-      isSuccess: false,
-      srcImg: australiaSrc,
-    },
-    {
-      id: 3,
-      name: "belgium",
-      isSuccess: false,
-      srcImg: belgiumSrc,
-    },
-    {
-      id: 4,
-      name: "france",
-      isSuccess: false,
-      srcImg: franceSrc,
-    },
-    {
-      id: 5,
-      name: "germany",
-      isSuccess: false,
-      srcImg: germanySrc,
-    },
-    {
-      id: 6,
-      name: "holland",
-      isSuccess: false,
-      srcImg: hollandSrc,
-    },
-    {
-      id: 7,
-      name: "japan",
-      isSuccess: false,
-      srcImg: japanSrc,
-    },
-    {
-      id: 8,
-      name: "poland",
-      isSuccess: false,
-      srcImg: polandSrc,
-    },
-    {
-      id: 9,
-      name: "portugal",
-      isSuccess: false,
-      srcImg: portugalSrc,
-    },
-    {
-      id: 10,
-      name: "korea",
-      isSuccess: false,
-      srcImg: koreaSrc,
-    },
-    {
-      id: 11,
-      name: "spain",
-      isSuccess: false,
-      srcImg: spainSrc,
-    },
-    {
-      id: 12,
-      name: "england",
-      isSuccess: false,
-      srcImg: englandSrc,
-    },
-    {
-      id: 13,
-      name: "switzerland",
-      isSuccess: false,
-      srcImg: switzerlandSrc,
-    },
-    {
-      id: 14,
-      name: "mexico",
-      isSuccess: false,
-      srcImg: mexicoSrc,
-    },
-    {
-      id: 15,
-      name: "usa",
-      isSuccess: false,
-      srcImg: usaSrc,
-    },
-    {
-      id: 16,
-      name: "wales",
-      isSuccess: false,
-      srcImg: walesSrc,
-    },
-  ]);
+  const [nations, setNations] = useState(DATA);
 
   const generateNation = () => {
-    let arr = nations.map((nation) => ({
-      ...nation,
-      rand: Math.random(),
-    }));
+    let arr = [];
+    if (nations.length !== DATA.length) {
+      arr = [...DATA].map((nation) => ({
+        ...nation,
+        rand: Math.random(),
+      }));
+      setSpan(4);
+      setStyles({ fontSize: "0.9em", marginBottom: -26 });
+    } else {
+      arr = nations.map((nation) => ({
+        ...nation,
+        rand: Math.random(),
+      }));
+    }
+
     arr = arr.sort((a, b) => a.rand - b.rand);
     for (let i = 0; i < NUMBER_NATION_SUCCESS; i++) {
       arr[i].isSuccess = true;
---
+++ src/data.js
@@ -0,0 +1,114 @@
+import argentinaSrc from "./flag/argentina-flag-8x5.gif";
+import australiaSrc from "./flag/australia-flag-8x5-std.jpg";
+import belgiumSrc from "./flag/belgium--flag-8x5.gif";
+import franceSrc from "./flag/france-flag-8x5.gif";
+import germanySrc from "./flag/germany-flag-8x5.gif";
+import hollandSrc from "./flag/holland-flag-8x5.gif";
+import japanSrc from "./flag/japan-flag-8x5.gif";
+import polandSrc from "./flag/poland-flag-8x5.jpg";
+import portugalSrc from "./flag/portugak-flag-8x5.jpg";
+import koreaSrc from "./flag/s-korea-8x5-std-1_1.jpg";
+import spainSrc from "./flag/spain-flag-8x5.jpg";
+import englandSrc from "./flag/st-george-flag-8x5.gif";
+import switzerlandSrc from "./flag/switzerland-flag-8x5.jpg";
+import mexicoSrc from "./flag/mexico-flag-8x5.gif";
+import usaSrc from "./flag/usa-flag-8x5.gif";
+import walesSrc from "./flag/wales-flag-8x5.jpg";
+export const data = [
+  {
+    id: 1,
+    name: "argentina",
+    isSuccess: false,
+    srcImg: argentinaSrc,
+  },
+  {
+    id: 2,
+    name: "australia",
+    isSuccess: false,
+    srcImg: australiaSrc,
+  },
+  {
+    id: 3,
+    name: "belgium",
+    isSuccess: false,
+    srcImg: belgiumSrc,
+  },
+  {
+    id: 4,
+    name: "france",
+    isSuccess: false,
+    srcImg: franceSrc,
+  },
+  {
+    id: 5,
+    name: "germany",
+    isSuccess: false,
+    srcImg: germanySrc,
+  },
+  {
+    id: 6,
+    name: "holland",
+    isSuccess: false,
+    srcImg: hollandSrc,
+  },
+  {
+    id: 7,
+    name: "japan",
+    isSuccess: false,
+    srcImg: japanSrc,
+  },
+  {
+    id: 8,
+    name: "poland",
+    isSuccess: false,
+    srcImg: polandSrc,
+  },
+  {
+    id: 9,
+    name: "portugal",
+    isSuccess: false,
+    srcImg: portugalSrc,
+  },
+  {
+    id: 10,
+    name: "korea",
+    isSuccess: false,
+    srcImg: koreaSrc,
+  },
+  {
+    id: 11,
+    name: "spain",
+    isSuccess: false,
+    srcImg: spainSrc,
+  },
+  {
+    id: 12,
+    name: "england",
+    isSuccess: false,
+    srcImg: englandSrc,
+  },
+  {
+    id: 13,
+    name: "switzerland",
+    isSuccess: false,
+    srcImg: switzerlandSrc,
+  },
+  {
+    id: 14,
+    name: "mexico",
+    isSuccess: false,
+    srcImg: mexicoSrc,
+  },
+  {
+    id: 15,
+    name: "usa",
+    isSuccess: false,
+    srcImg: usaSrc,
+  },
+  {
+    id: 16,
+    name: "wales",
+    isSuccess: false,
+    srcImg: walesSrc,
+  },
+];
